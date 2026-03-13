e916f8352a0a8199e2850458119b235922f186bf
Suraj-harry <suraj18316@gmail.com>
2026-03-13 11:17:41 +0000

    part4

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
-import Cards_data from '../../assets/cards/Cards_data'
+import cards_data from '../../assets/cards/Cards_data.js'
 import { Link } from 'react-router-dom'
 import { TMDB_Access_Key } from '../../config'
 
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // replace below firebase config with your own
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCEy6YUeAvjOmRARvTRdfGeL8RJDzs8U7A",
+    apiKey: "0f99b079a6f10c3bedad7a295a957983",
     authDomain: "netflix-clone-d1fa8.firebaseapp.com",
     projectId: "netflix-clone-d1fa8",
     storageBucket: "netflix-clone-d1fa8.appspot.com",
@@ -14,7 +14,7 @@ const firebaseConfig = {
 // craete a/c on themoviedb.org and replace access token below
 
 
-const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZTNlNzczZDIwY2M2Y2NhNWQ4YWVjMjQzNTdlNDc1ZCIsInN1YiI6IjY2MjIyMDFhYWUzODQzMDE4NzJhNTJjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vPDW2QwNr9hIRdOvJx_x8hbHnDYZGHMtnZwfkqb3J8U";
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZjk5YjA3OWE2ZjEwYzNiZWRhZDdhMjk1YTk1Nzk4MyIsIm5iZiI6MTc1Mzc2NDY2Mi4wMDQsInN1YiI6IjY4ODg1MzM1NDk5M2YyNDk2ODI0NzVkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VHvBVD555autGEQ_tKFeMYucL6C8IyAmsufcJMHnM-M";
 
 
   export {firebaseConfig, TMDB_Access_Key};
