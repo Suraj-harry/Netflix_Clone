7f903b926e967338652cad1ff7b76c35f0b22242
Suraj-harry <suraj18316@gmail.com>
2026-03-13 12:29:55 +0000

    change1

--- src/config.js
+++ src/config.js
@@ -1,18 +1,16 @@
 import { initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBKmfqvij-PTgmQlalj7UE0dk-dfJAz_Tc",
   authDomain: "netflix-clone-9ddfb.firebaseapp.com",
   projectId: "netflix-clone-9ddfb",
   storageBucket: "netflix-clone-9ddfb.firebasestorage.app",
   messagingSenderId: "290452787476",
-  appId: "1:290452787476:web:2af20b4eff6dcfd6447201"
+  appId: "1:290452787476:web:110138fffb3ceab6447201"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZjk5YjA3OWE2ZjEwYzNiZWRhZDdhMjk1YTk1Nzk4MyIsIm5iZiI6MTc1Mzc2NDY2Mi4wMDQsInN1YiI6IjY4ODg1MzM1NDk5M2YyNDk2ODI0NzVkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VHvBVD555autGEQ_tKFeMYucL6C8IyAmsufcJMHnM-M";
 
